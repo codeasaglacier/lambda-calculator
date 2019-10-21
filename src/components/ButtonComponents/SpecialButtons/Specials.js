102ddb56b1a303c35e22429e254bad360364d2e3
codeasaglacier <remingtonshock@gmail.com>
2019-10-21 15:00:35 -0800

    pausing for class

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -2,11 +2,13 @@ import React from "react";
 
 //import any components needed
 
+import { specials } from '../../../data'
+
 //Import your array data to from the provided data file
 
 const Specials = () => {
   // STEP 2 - add the imported data to state
-
+  const [specialsState, setSpecialsState] = useState(specials)
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
